04b5ee38681c8222bd8027809361cc7c307a38d7
dezsim <dezsimartin@gmail.com>
2022-04-21 20:06:23 +0200

    optional chaining and trycatch temp for interactions

--- src/discords/Button.ts
+++ src/discords/Button.ts
@@ -2,23 +2,32 @@
 import { ButtonInteraction } from "discord.js";
 import { ButtonComponent, Discord } from "discordx";
 import { join } from "../commands";
+import logger from "../utils/logger";
 
 @Discord()
 abstract class Buttons {
   @ButtonComponent("join-button")
   async button1(interaction: ButtonInteraction) {
-    await interaction.reply({
-      content: "I'll update your accesses as soon as possible.",
-      ephemeral: true,
-    });
+    try {
+      await interaction.reply({
+        content: "I'll update your accesses as soon as possible.",
+        ephemeral: true,
+      });
+    } catch (error) {
+      logger.verbose(`join-button interaction reply ${error.message}`);
+    }
 
     const message = await join(
-      interaction.user.id,
-      interaction.guild,
-      interaction.token
+      interaction?.user.id,
+      interaction?.guild,
+      interaction?.token
     );
 
-    await interaction.editReply(message);
+    try {
+      await interaction.editReply(message);
+    } catch (error) {
+      logger.verbose(`join-button interaction EDITREPLY ${error.message}`);
+    }
   }
 }
 
