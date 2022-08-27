e026cb5a6e67f499b29f16311ed06a2e56194bbd
DevidAbt <devid@agora.xyz>
2022-08-27 21:01:50 +0200

    Removed rate limit from join button reply

--- src/discords/Button.ts
+++ src/discords/Button.ts
@@ -10,17 +10,15 @@ import logger from "../utils/logger";
 @Discord()
 abstract class Buttons {
   @ButtonComponent("join-button")
-  async button1(interaction: ButtonInteraction) {
+  async joinButton(interaction: ButtonInteraction) {
     logger.debug(
       `join-trace ${interaction.user?.id} ${interaction.guildId} button pressed`
     );
     try {
-      await sendMessageLimiter.schedule(() =>
-        interaction.reply({
-          content: "I'll update your accesses as soon as possible.",
-          ephemeral: true,
-        })
-      );
+      await interaction.reply({
+        content: "I'll update your accesses as soon as possible.",
+        ephemeral: true,
+      });
     } catch (error) {
       logger.verbose(`join-button interaction reply ${error.message}`);
       return;
