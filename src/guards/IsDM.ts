eaa10eab3d9051809a32b3cd2411fa5681b25f4b
0xAndrewBlack <lemon@zgen.hu>
2022-10-06 19:06:09 +0200

    refactor: Guards updated.

--- src/guards/IsDM.ts
+++ src/guards/IsDM.ts
@@ -1,13 +1,15 @@
+import { ChannelType } from "discord.js";
 import { ArgsOf, GuardFunction, SimpleCommandMessage } from "discordx";
 
 const IsDM: GuardFunction<
   ArgsOf<"messageCreate"> | SimpleCommandMessage
 > = async (message, _, next) => {
-  if (
+  const msgIsInDM =
     message instanceof SimpleCommandMessage
-      ? message?.message.channel.type === "DM"
-      : message[0].channel.type === "DM"
-  ) {
+      ? message?.message.channel.type === ChannelType.DM
+      : message[0].channel.type === ChannelType.DM;
+
+  if (msgIsInDM) {
     await next();
   }
 };
