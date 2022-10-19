5ce3bcf655b18725f7fd87f29f816a64a1dfee34
ejay <51904825+ejayxyz@users.noreply.github.com>
2022-10-19 13:47:13 +0200

    Added entity too large handler (#364)
    * added entity too large handler
    
    * refactored error method

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,11 +1,18 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import config from "../config";
 import logger from "../utils/logger";
+import { getErrorResult } from "../utils/utils";
 import router from "./router";
 
 const createApi = () => {
   const api = express();
   api.disable("x-powered-by");
+
+  api.use((err: any, _req: Request, res: Response) => {
+    res
+      .status(err.type === "entity.too.large" ? 413 : 500)
+      .json(getErrorResult(err.message));
+  });
 
   api.use(express.json({ limit: "6mb" }));
   api.use(config.api.prefix, router());
